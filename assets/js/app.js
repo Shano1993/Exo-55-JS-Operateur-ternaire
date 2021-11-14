d591fea23f4cc38cb78ab8762d9668b804f4425d
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-14 15:06:46 +0100

    js

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+
+let firstelement = document.getElementById('first')
+firstelement.style.color = "red"
+firstelement.style.background = "black"
+
+let valueOne = 5;
+valueOne = document.getElementById('value_one').innerText = "5"
+
+let valueTwo = 12;
+valueTwo = document.getElementById('value_two').innerText = "12"
+
+let result = 17;
+
+if(valueTwo+valueTwo) {
+    result = document.getElementById('resultat').innerText ="17"
+}
+
+else {
+    result = document.getElementById('resultat').innerText ="Incorrect"
+}
+
+if( (result-10) > 0) {
+    firstelement = document.getElementById('first').innerText ="Le résultat n'est strictement pas inférieure à 0"
+}
+
+
+
+
+
+
+
+
+
+
